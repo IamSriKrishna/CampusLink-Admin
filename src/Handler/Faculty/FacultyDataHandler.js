ed3b02ba50fbcaf9f5c3916b63a4b3e71c7c8d22
Sri <j.krithick@gmail.com>
2023-10-31 12:13:26 +0530

    FacultyDataHandler fix

--- src/Handler/Faculty/FacultyDataHandler.js
+++ src/Handler/Faculty/FacultyDataHandler.js
@@ -1,16 +1,11 @@
 const FacultyModel = require("../../Model/Faculty");
 
 const getFacultyData = async (req, res) => {
-  try {
-    const faculty = await FacultyModel.findById(req.faculty);
-    console.log("GetFaculty data:" + faculty);
-    if (!faculty) {
-      return res.status(400).json({ msg: "No Faculty data exist!" });
-    }
-    res.json({ ...faculty._doc, token: req.token });
-  } catch (e) {
-    console.log(e);
+  const user = await FacultyModel.findById(req.user);
+  if (!user) {
+    return res.status(400).json({ msg: "No Faculty data exist!" });
   }
+  res.json({ ...user._doc, token: req.token });
 };
 
 module.exports = {
