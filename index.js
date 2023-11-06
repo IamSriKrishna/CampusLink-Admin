4137d5b6a5d6be336f5fa1b73b23f2cd46bce5f8
IamSriKrishna <srik090704@gmail.com>
2023-11-06 23:38:21 +0530

    Socket Io Added

--- index.js
+++ index.js
@@ -114,72 +114,72 @@ app.listen(process.env.PORT, () => {
   console.log(`Successfully Connected to the Port: ${process.env.PORT}`);
 });
 
-// const io = require('socket.io')(server, {
-//   pingTimeout: 60000,
-//   cors: {
-//       origin: "http://13.126.172.50:3000"
-//   }
-// });
+const io = require('socket.io')(server, {
+  pingTimeout: 60000,
+  cors: {
+      origin: "http://13.126.172.50:3000"
+  }
+});
 
-// io.on("connection", (socket) => {
-//   console.log("connected to sockets");
+io.on("connection", (socket) => {
+  console.log("connected to sockets");
 
-//   socket.on('setup', (userId) => {
-//       socket.join(userId);
-//       socket.broadcast.emit('online-user', userId)
-//       console.log(userId);
-//   });
-
-
-//   socket.on('typing', (room) => {
-//       // console.log("typing");
-//       // console.log("room");
-//       socket.to(room).emit('typing', room)
-//   });
+  socket.on('setup', (userId) => {
+      socket.join(userId);
+      socket.broadcast.emit('online-user', userId)
+      console.log(userId);
+  });
 
 
-//   socket.on('stop typing', (room) => {
-//       // console.log("stop typing");
-//       // console.log("room");
-//       socket.to(room).emit('stop typing', room)
-//   });
+  socket.on('typing', (room) => {
+      // console.log("typing");
+      // console.log("room");
+      socket.to(room).emit('typing', room)
+  });
 
 
-//   socket.on('join chat', (room) => {
-//       socket.join(room)
-//       // console.log('User Joined : ' + room);
-//   });
-
-//   socket.on('new message', (newMessageReceived) => {
-
-//       var chat = newMessageReceived.chat;
-//       var room = chat._id;
-
-//       var sender = newMessageReceived.sender;
-
-//       if (!sender || sender._id) {
-//           // console.log('Sender not defined');
-//           return;
-//       }
-
-//       var senderId = sender._id;
-//       // console.log(senderId + "message sender");
-//       const users = chat.users;
-
-//       if (!users) {
-//           // console.log(" Users not defined");
-//           return;
-//       }
-
-//       socket.to(room).emit('message received', newMessageReceived);
-//       socket.to(room).emit('message sent', "New Message");
-//   });
+  socket.on('stop typing', (room) => {
+      // console.log("stop typing");
+      // console.log("room");
+      socket.to(room).emit('stop typing', room)
+  });
 
 
-//   socket.off('setup', () => {
-//       // console.log('user offline');
-//       socket.leave(userId)
-//   })
+  socket.on('join chat', (room) => {
+      socket.join(room)
+      // console.log('User Joined : ' + room);
+  });
+
+  socket.on('new message', (newMessageReceived) => {
+
+      var chat = newMessageReceived.chat;
+      var room = chat._id;
+
+      var sender = newMessageReceived.sender;
+
+      if (!sender || sender._id) {
+          // console.log('Sender not defined');
+          return;
+      }
+
+      var senderId = sender._id;
+      // console.log(senderId + "message sender");
+      const users = chat.users;
+
+      if (!users) {
+          // console.log(" Users not defined");
+          return;
+      }
+
+      socket.to(room).emit('message received', newMessageReceived);
+      socket.to(room).emit('message sent', "New Message");
+  });
 
 
-// })
+  socket.off('setup', () => {
+      // console.log('user offline');
+      socket.leave(userId)
+  })
+
+
+})
