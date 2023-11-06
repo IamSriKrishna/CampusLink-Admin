24988dad554d78fde4d067e417822a42ac2a6c75
IamSriKrishna <srik090704@gmail.com>
2023-11-06 22:51:02 +0530

    Backend done for messaging

---
+++ src/Handler/Messenger/MessengerAuthHandler.js
@@ -0,0 +1,64 @@
+const Chat = require("../../Model/chat");
+const Message = require("../../Model/message");
+const User = require("../../Model/Student");
+const getAllMessage = async (req, res) => {
+    try {
+        const pageSize = 12; // Number of messages per page
+        const page = req.query.page || 1; // Current page number
+
+        // Calculate the number of messages to skip
+        const skipMessages = (page - 1) * pageSize;
+
+        // Find messages with pagination
+        var messages = await Message.find({ chat: req.params.id })
+            .populate("sender", "name dp rollno")
+            .populate("chat")
+            .sort({ createdAt: -1 }) // Sort messages by descending createdAt
+            .skip(skipMessages) // Skip the messages based on pagination
+            .limit(pageSize); // Limit the number of messages per page
+
+        messages = await User.populate(messages, {
+            path: "chat.users",
+            select: "name dp rollno",
+        });
+
+        res.json(messages);
+    } catch (error) {
+        res.status(500).json('Could not get chats');
+    }
+}
+
+const sendMessage = async (req, res) => {
+    const { content, chatId, receiver } = req.body;
+
+        if (!content || !chatId) {
+            console.log("Invalid data passed into request");
+            return res.status(400);
+        }
+
+        var newMessage = {
+            sender: req.user.id,
+            content: content,
+            receiver: receiver,
+            chat: chatId,
+        };
+
+        try {
+            var message = await Message.create(newMessage);
+
+            message = await message.populate("sender", "name dp rollno");
+            message = await message.populate("chat");
+            message = await User.populate(message, {
+                path: "chat.users",
+                select: "name dp rollno",
+            });
+
+            await Chat.findByIdAndUpdate(req.body.chatId, { latestMessage: message });
+
+            res.json(message);
+        } catch (error) {
+            res.status(400).json(error);
+        }
+}
+
+module.exports = {getAllMessage,sendMessage}
