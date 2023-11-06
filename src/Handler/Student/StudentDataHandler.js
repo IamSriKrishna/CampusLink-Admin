62f941f6bc8250079f8f48c1ba0f86ac304a879b
IamSriKrishna <srik090704@gmail.com>
2023-11-07 03:11:25 +0530

    Error Resolved"

--- src/Handler/Student/StudentDataHandler.js
+++ src/Handler/Student/StudentDataHandler.js
@@ -1,11 +1,9 @@
 const StudentModel = require("../../Model/Student");
 
 const getUserData = async (req, res) => {
-  const user = await StudentModel.findById(req.user);
-  if (!user) {
-    return res.status(400).json({ msg: "No Student data exist!" });
-  }
-  res.json({ ...user._doc, token: req.token });
+  const user = await StudentModel.findById(req.user.id);
+  const { password, __v, createdAt, ...userdata } = user._doc;
+  res.status(200).json(userdata)
 };
 
 module.exports = {
--- src/Router/Student/StudentDataRouter.js
+++ src/Router/Student/StudentDataRouter.js
@@ -3,13 +3,13 @@ const express = require("express");
 
 const { getUserData } = require("../../Handler/Student/StudentDataHandler");
 // Router
-const auth = require("../../../middleware/Auth");
+const {verifyTokenAndAuthorization} = require("../../../middleware/Authentication");
 
 // INIT
 const StudentRouter = express.Router();
 
 // get user data
-StudentRouter.get("/", auth, async function (req, res) {
+StudentRouter.get("/", verifyTokenAndAuthorization, async function (req, res) {
   // return res.json(msg: "Hello")
   await getUserData(req, res);
 });
