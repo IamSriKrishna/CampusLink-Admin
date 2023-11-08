b605b3daf1950e39996b6f07e19e76f061f481e5
IamSriKrishna <srik090704@gmail.com>
2023-11-08 20:32:33 +0530

    notification error solved

--- src/Handler/chat/ChatAuthHandler.js
+++ src/Handler/chat/ChatAuthHandler.js
@@ -9,41 +9,43 @@ const accessChat = async (req, res) => {
         return res.status(400).json("UserId param not sent with the request");
     }
 
-    if (!req.user || !req.user.id) {
-        console.log("User is not authenticated or user ID is missing");
-        return res.status(401).json("User is not authenticated");
-    }
-
-    // Check if an existing chat exists with the user making the request and the specified userId
-    var isExistingChat = await Chat.find({
+    // Check if a chat with the given userId exists
+    var isChat = await Chat.find({
+        chatName: req.user.id, // Ensure chatName is correctly set
+        isGroupChat: false,
         $and: [
-            {
-                chatName: req.user.id, // Ensure chatName is correctly set
-                isGroupChat: false,
-                users: { $elemMatch: { $eq: req.user.id } },
-            },
-            {
-                users: { $elemMatch: { $eq: userId } },
-            },
+            { users: { $elemMatch: { $eq: req.user.id||userId } } },
+            { users: { $elemMatch: { $eq: userId||req.user.id } } },
         ],
     })
         .populate("users", "-password");
 
-    isExistingChat = await User.populate(isExistingChat, {
+    isChat = await User.populate(isChat, {
         path: "latestMessage.sender",
         select: "name dp rollno fcmtoken",
     });
+    
+    if (!userId) {
+        console.log("UserId param not sent with the request");
+        return res.status(400).json("UserId param not sent with the request");
+    }
 
-    if (isExistingChat.length > 0) {
-        // Chat already exists, send the chat information
-        res.status(200).json(isExistingChat[0]);
+    if (!req.user || !req.user.id) {
+        console.log("User is not authenticated or user ID is missing");
+        return res.status(401).json("User is not authenticated");
+    }
+    if (isChat.length > 0) {
+        // Chat exists, send the chat information
+        res.status(200).json(isChat[0]);
+        console.log(isChat[0]);
     } else {
-        // No existing chat found, create a new one
+    
         var chatData = {
-            chatName: req.user.id,
+            chatName: req.user.id, 
             isGroupChat: false,
             users: [req.user.id, userId],
         };
+        console.log(chatData); // Check if chatName is set correctly
 
         try {
             const createdChat = await Chat.create(chatData);
@@ -51,9 +53,10 @@ const accessChat = async (req, res) => {
                 "users",
                 "-password"
             );
-
+            console.log(FullChat)
             res.status(200).json(FullChat);
         } catch (error) {
+            console.log(error)
             res.status(400).json("Error Creating Chat");
         }
     }
@@ -61,33 +64,26 @@ const accessChat = async (req, res) => {
 
 const getChats = async (req, res) => {
     try {
-        const chats = await Chat.find({ users: { $elemMatch: { $eq: req.user.id } } })
+        Chat.find({ users: { $elemMatch: { $eq: req.user.id } } })
             .populate("users", "-password")
             .populate("groupAdmin", "-password")
-            .sort({ updatedAt: -1 });
-
-        // Check if there are any existing chats with the user making the request and the provided userId
-        for (const chat of chats) {
-            if (chat.users.includes(req.user.id) && chat.users.includes(userId)) {
-                // Chat with both users already exists, skip this chat
-                continue;
-            }
-
-            // Chat with both users does not exist, populate additional details
-            await User.populate(chat, {
-                path: "latestMessage.sender",
-                select: "name dp rollno fcmtoken",
+            .sort({ updatedAt: -1 })
+            .then(async (results) => {
+                results = await User.populate(results, {
+                    path: "latestMessage.sender",
+                    select: "name dp rollno fcmtoken",
+                });
+                
+                console.log(results)
+                res.status(200).send(results);
             });
-        }
-
-        console.log(chats)
-        res.status(200).send(chats);
     } catch (error) {
         console.log(error)
         res.status(500).json("Error Fetching Chat");
-    }
-};
 
+    }
+
+}
 module.exports = {
     accessChat,
     getChats
