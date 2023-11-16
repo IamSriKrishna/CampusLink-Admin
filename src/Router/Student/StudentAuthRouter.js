6f952aeecfe36d4cae2036b07dc2b3b8f97a2678
Sri <j.krithick@gmail.com>
2023-11-16 14:11:24 +0530

    Sign up endpoint commit

--- src/Router/Student/StudentAuthRouter.js
+++ src/Router/Student/StudentAuthRouter.js
@@ -8,7 +8,7 @@ const {
   TokenValid,
   getAllStudentData,
   UpdateFcmToken,
-  searchStudentByName
+  searchStudentByName,
 } = require("../../Handler/Student/StudentAuthHandler");
 
 const {
@@ -19,8 +19,8 @@ const {
   getFollowersCount,
   getFollowingCount,
   getFollowers,
-  getFollowing
-} = require("../FollowingorFollowers/FollowingORFollowers.js")
+  getFollowing,
+} = require("../FollowingorFollowers/FollowingORFollowers.js");
 // INIT
 const StudentRouter = express.Router();
 
@@ -31,7 +31,7 @@ StudentRouter.use((err, req, res, next) => {
 });
 
 //signup endpoint
-StudentRouter.post("/kcg/student/signup", async function (req, res) {
+StudentRouter.post("/kcg/student/SignUp", async function (req, res) {
   await studentSignUp(req, res);
 });
 //Signin endpoint
@@ -50,14 +50,18 @@ StudentRouter.post("/tokenIsValid", async function (req, res) {
 StudentRouter.put("/kcg/student/fcm-token/:id", async function (req, res) {
   await UpdateFcmToken(req, res);
 });
-//Get All Student 
+//Get All Student
 StudentRouter.get("/kcg/student/getAllStudentData", async function (req, res) {
   await getAllStudentData(req, res);
 });
-//Get All Student 
-StudentRouter.get("/students/search", searchStudentByName, async function (req, res) {
-  await searchStudentByName(req, res);
-});
+//Get All Student
+StudentRouter.get(
+  "/students/search",
+  searchStudentByName,
+  async function (req, res) {
+    await searchStudentByName(req, res);
+  }
+);
 
 StudentRouter.post("/students/addfollowers", async function (req, res) {
   const { studentId, followerId } = req.body; // Assuming the IDs are sent in the request body
@@ -69,14 +73,13 @@ StudentRouter.post("/students/addfollowers", async function (req, res) {
   }
 });
 
-
 StudentRouter.post("/students/addFollowing", async function (req, res) {
   const { studentId, followingId } = req.body; // Assuming the IDs are sent in the request body
   const result = await addFollowing(studentId, followingId);
   if (result) {
     res.status(200).json({ message: "Following added successfully" });
   } else {
-    console.error("Error")
+    console.error("Error");
     res.status(400).json({ message: "Failed to add follower" });
   }
 });
@@ -84,54 +87,72 @@ StudentRouter.post("/students/addFollowing", async function (req, res) {
 StudentRouter.post("/students/removeFromFollowing", async function (req, res) {
   const { studentId, followingId } = req.body; // Assuming the IDs are sent in the request body
   const removed = await removeFromFollowing(studentId, followingId);
-if (removed) {
-    console.log('Student removed from following successfully.');
-    res.status(200).json({ message: "Student removed from following successfully." });
-} else {
-  
-  console.error("Error")
-  res.status(400).json({ message: "Failed to remove student from following." });
-    console.log('Failed to remove student from following.');
-}
+  if (removed) {
+    console.log("Student removed from following successfully.");
+    res
+      .status(200)
+      .json({ message: "Student removed from following successfully." });
+  } else {
+    console.error("Error");
+    res
+      .status(400)
+      .json({ message: "Failed to remove student from following." });
+    console.log("Failed to remove student from following.");
+  }
 });
 
 StudentRouter.post("/students/removeFromfollowers", async function (req, res) {
   const { studentId, followersId } = req.body; // Assuming the IDs are sent in the request body
   const removed = await removeFromfollowers(studentId, followersId);
-if (removed) {
-    console.log('Student removed from followers successfully.');
-    res.status(200).json({ message: "Student removed from followers successfully." });
-} else {
-  
-  console.error("Error")
-  res.status(400).json({ message: "Failed to remove student from followers." });
-    console.log('Failed to remove student from followers.');
-}
+  if (removed) {
+    console.log("Student removed from followers successfully.");
+    res
+      .status(200)
+      .json({ message: "Student removed from followers successfully." });
+  } else {
+    console.error("Error");
+    res
+      .status(400)
+      .json({ message: "Failed to remove student from followers." });
+    console.log("Failed to remove student from followers.");
+  }
 });
 
 // Endpoint to get total followers count for a student
-StudentRouter.get("/students/followersCount/:studentId", async function (req, res) {
-  const studentId = req.params.studentId;
-  const followersCount = await getFollowersCount(studentId);
-  res.status(200).json({ followersCount });
-});
+StudentRouter.get(
+  "/students/followersCount/:studentId",
+  async function (req, res) {
+    const studentId = req.params.studentId;
+    const followersCount = await getFollowersCount(studentId);
+    res.status(200).json({ followersCount });
+  }
+);
 
 // Endpoint to get total following count for a student
-StudentRouter.get("/students/followingCount/:studentId", async function (req, res) {
-  const studentId = req.params.studentId;
-  const followingCount = await getFollowingCount(studentId);
-  res.status(200).json({ followingCount });
-});
+StudentRouter.get(
+  "/students/followingCount/:studentId",
+  async function (req, res) {
+    const studentId = req.params.studentId;
+    const followingCount = await getFollowingCount(studentId);
+    res.status(200).json({ followingCount });
+  }
+);
 
-StudentRouter.get("/students/getFollowers/:studentId", async function (req, res) {
-  const studentId = req.params.studentId;
-  const followers = await getFollowers(studentId);
-  res.status(200).json({ followers });
-});
+StudentRouter.get(
+  "/students/getFollowers/:studentId",
+  async function (req, res) {
+    const studentId = req.params.studentId;
+    const followers = await getFollowers(studentId);
+    res.status(200).json({ followers });
+  }
+);
 
-StudentRouter.get("/students/getFollowing/:studentId", async function (req, res) {
-  const studentId = req.params.studentId;
-  const following = await getFollowing(studentId);
-  res.status(200).json({ following });
-});
+StudentRouter.get(
+  "/students/getFollowing/:studentId",
+  async function (req, res) {
+    const studentId = req.params.studentId;
+    const following = await getFollowing(studentId);
+    res.status(200).json({ following });
+  }
+);
 module.exports = StudentRouter;
