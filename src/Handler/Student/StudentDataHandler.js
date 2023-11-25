a79e7582f346b90cfd0cf6b4baa66c4e0e88ff1f
Sri <j.krithick@gmail.com>
2023-11-25 14:10:31 +0530

    GetAllStudent data endpoint create

--- src/Handler/Student/StudentDataHandler.js
+++ src/Handler/Student/StudentDataHandler.js
@@ -8,6 +8,22 @@ const getUserData = async (req, res) => {
   res.json({ ...user._doc, token: req.token });
 };
 
+const getAllStudentData = async (req, res) => {
+  try {
+    const students_data = await StudentModel.find();
+    if (!students_data) {
+      console.log("Student Data not found");
+    } else {
+      res.json(students_data);
+    }
+  } catch (e) {
+    //console.log(e)
+    console.error(e);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
 module.exports = {
   getUserData,
+  getAllStudentData,
 };
--- src/Router/Student/StudentDataRouter.js
+++ src/Router/Student/StudentDataRouter.js
@@ -1,7 +1,10 @@
 //Handlers
 const express = require("express");
 
-const { getUserData } = require("../../Handler/Student/StudentDataHandler");
+const {
+  getUserData,
+  getAllStudentData,
+} = require("../../Handler/Student/StudentDataHandler");
 // Router
 const auth = require("../../../middleware/Auth");
 
@@ -10,8 +13,11 @@ const StudentRouter = express.Router();
 
 // get user data
 StudentRouter.get("/", auth, async function (req, res) {
-  // return res.json(msg: "Hello")
   await getUserData({ user: req.user, token: req.token }, res);
+});
+
+StudentRouter.get("/kcg/students", auth, async function (req, res) {
+  await getAllStudentData({ user: req.user, token: req.token }, res);
 });
 
 module.exports = StudentRouter;
